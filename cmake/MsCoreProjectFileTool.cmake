b06c38f20dda0768e4ccd0ef99ca2b45ecfeb6d2
petitechose-audio <contact@petitechose.audio>
2026-06-21 20:20:00 +0200

    build: share core native source groups

--- cmake/MsCoreProjectFileTool.cmake
+++ cmake/MsCoreProjectFileTool.cmake
@@ -1,8 +1,10 @@
 function(ms_core_add_project_file_tool)
+    if(NOT DEFINED MS_CORE_PROJECT_FILE_CORE_SOURCES)
+        message(FATAL_ERROR "ms_core_add_project_file_tool requires cmake/MsCoreSources.cmake")
+    endif()
+
     add_library(ms_core_project_file_open_control_native STATIC
-        "${MS_CORE_OC_FRAMEWORK_DIR}/src/oc/state/NotificationQueue.cpp"
-        "${MS_CORE_OC_NOTE_DIR}/src/oc/note/sequencer/StepSequencerGraph.cpp"
-        "${MS_CORE_OC_NOTE_DIR}/src/oc/note/sequencer/StepSequencerState.cpp")
+        ${MS_CORE_PROJECT_FILE_OPEN_CONTROL_SOURCES})
     target_include_directories(ms_core_project_file_open_control_native
         PUBLIC
             "${MS_CORE_OC_FRAMEWORK_DIR}/src"
@@ -10,37 +12,7 @@ function(ms_core_add_project_file_tool)
     target_compile_features(ms_core_project_file_open_control_native PUBLIC cxx_std_20)
 
     add_library(ms_core_project_file_native STATIC
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/persistence/ProjectChunkMigration.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/persistence/ProjectFileContainer.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/persistence/ProjectMigration.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/persistence/ProjectSnapshotPersistenceCodec.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/persistence/ProjectStatePersistenceCodec.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/persistence/SequencerPersistenceCodec.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/persistence/SequencerPersistenceEnvelope.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/MacroEditState.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/StructureClipboardPastePlan.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/StructureClipboardState.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/StructureSelectionState.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/macro/MacroPagesState.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/project/ProjectSnapshotLifecycle.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/project/ProjectSlug.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/project/ProjectState.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerContentProjectionOps.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerContentStepOps.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerContentViewInternal.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerContentViewOps.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerGraphChildOps.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerGraphContentOps.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerGraphOps.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerGraphPropertyOps.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerHistory.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerSnapshotOps.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerState.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerStructureHistory.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerTrackBankOps.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerTrackBankState.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/sequencer/SequencerUiState.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/state/shared/StructureSlotOps.cpp")
+        ${MS_CORE_PROJECT_FILE_CORE_SOURCES})
     target_compile_definitions(ms_core_project_file_native PUBLIC OC_LOG)
     target_include_directories(ms_core_project_file_native
         PUBLIC
---
+++ cmake/MsCoreSources.cmake
@@ -0,0 +1,77 @@
+get_filename_component(MS_CORE_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
+set(MS_CORE_SOURCE_ROOT "${MS_CORE_ROOT_DIR}/src")
+
+set(MS_CORE_NATIVE_SOURCE_PATTERNS
+    "${MS_CORE_SOURCE_ROOT}/state/*.cpp"
+    "${MS_CORE_SOURCE_ROOT}/protocol/*.cpp"
+    "${MS_CORE_SOURCE_ROOT}/persistence/*.cpp"
+    "${MS_CORE_SOURCE_ROOT}/sequencer/*.cpp"
+    "${MS_CORE_SOURCE_ROOT}/validation/ux/*.cpp"
+    "${MS_CORE_SOURCE_ROOT}/handler/common/*.cpp"
+    "${MS_CORE_SOURCE_ROOT}/handler/macro/*.cpp"
+    "${MS_CORE_SOURCE_ROOT}/handler/project/*.cpp"
+    "${MS_CORE_SOURCE_ROOT}/handler/sequencer/*.cpp"
+    "${MS_CORE_SOURCE_ROOT}/handler/settings/*.cpp"
+    "${MS_CORE_SOURCE_ROOT}/handler/transport/*.cpp"
+    "${MS_CORE_SOURCE_ROOT}/handler/view/*.cpp")
+
+file(GLOB_RECURSE MS_CORE_NATIVE_SOURCES CONFIGURE_DEPENDS
+    ${MS_CORE_NATIVE_SOURCE_PATTERNS})
+
+set(MS_CORE_NATIVE_EXTRA_SOURCES
+    "${MS_CORE_SOURCE_ROOT}/context/standalone/SequencerEncoderSyncCoordinator.cpp"
+    "${MS_CORE_SOURCE_ROOT}/ui/common/GlobalTrackNavigationStripModel.cpp"
+    "${MS_CORE_SOURCE_ROOT}/ui/sequencer/StepContentBadgeProjection.cpp")
+
+list(APPEND MS_CORE_NATIVE_SOURCES ${MS_CORE_NATIVE_EXTRA_SOURCES})
+
+set(MS_CORE_PROJECT_FILE_OPEN_CONTROL_SOURCES
+    "${MS_CORE_OC_FRAMEWORK_DIR}/src/oc/state/NotificationQueue.cpp"
+    "${MS_CORE_OC_NOTE_DIR}/src/oc/note/sequencer/StepSequencerGraph.cpp"
+    "${MS_CORE_OC_NOTE_DIR}/src/oc/note/sequencer/StepSequencerState.cpp")
+
+set(MS_CORE_PROJECT_FILE_CORE_SOURCES
+    "${MS_CORE_SOURCE_ROOT}/persistence/ProjectChunkMigration.cpp"
+    "${MS_CORE_SOURCE_ROOT}/persistence/ProjectFileContainer.cpp"
+    "${MS_CORE_SOURCE_ROOT}/persistence/ProjectMigration.cpp"
+    "${MS_CORE_SOURCE_ROOT}/persistence/ProjectSnapshotPersistenceCodec.cpp"
+    "${MS_CORE_SOURCE_ROOT}/persistence/ProjectStatePersistenceCodec.cpp"
+    "${MS_CORE_SOURCE_ROOT}/persistence/SequencerPersistenceCodec.cpp"
+    "${MS_CORE_SOURCE_ROOT}/persistence/SequencerPersistenceEnvelope.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/MacroEditState.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/StructureClipboardPastePlan.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/StructureClipboardState.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/StructureSelectionState.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/macro/MacroPagesState.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/project/ProjectSnapshotLifecycle.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/project/ProjectSlug.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/project/ProjectState.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerContentProjectionOps.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerContentStepOps.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerContentViewInternal.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerContentViewOps.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerGraphChildOps.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerGraphContentOps.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerGraphOps.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerGraphPropertyOps.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerHistory.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerSnapshotOps.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerState.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerStructureHistory.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerTrackBankOps.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerTrackBankState.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/sequencer/SequencerUiState.cpp"
+    "${MS_CORE_SOURCE_ROOT}/state/shared/StructureSlotOps.cpp")
+
+function(ms_core_assert_sources_exist)
+    foreach(source IN LISTS ARGN)
+        if(NOT EXISTS "${source}")
+            message(FATAL_ERROR "Core source not found: ${source}")
+        endif()
+    endforeach()
+endfunction()
+
+ms_core_assert_sources_exist(
+    ${MS_CORE_NATIVE_EXTRA_SOURCES}
+    ${MS_CORE_PROJECT_FILE_OPEN_CONTROL_SOURCES}
+    ${MS_CORE_PROJECT_FILE_CORE_SOURCES})
