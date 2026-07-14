9ea95f1b8704de232ec768e65e1cead9c01777b0
petitechose-audio <contact@petitechose.audio>
2026-07-14 03:20:44 +0200

    feat(ui): add semantic action and state icons

--- src/ui/font/data/standalone_icons_12.hpp
+++ src/ui/font/data/standalone_icons_12.hpp
@@ -1,4 +1,4 @@
-// Auto-generated | standalone_icons | 12px | 4bpp | 2026-06-22 23:04
+// Auto-generated | standalone_icons | 12px | 4bpp | 2026-07-14 03:11
 #pragma once
 #include "config/PlatformCompat.hpp"
 extern const uint8_t standalone_icons_12_bin[] PROGMEM;
